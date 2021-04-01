97c5bb86c2646bfaa288922d4055b73829b8fe90
sspangsberg <smsj@easv.dk>
2021-04-01 16:55:30 +0200

    Test update

--- server.js
+++ server.js
@@ -16,3 +16,5 @@ const PORT = process.env.PORT || 4000;
 app.listen(PORT, function () {
     console.log("Server is running on port: " + PORT);
   });
+
+module.exports = app;
--- test/workflow.test.js
+++ test/workflow.test.js
@@ -1,7 +1,7 @@
+const chai = require('chai');
 const expect = chai.expect;
-const should = chai.should();
 const chaiHttp = require('chai-http');
-const server = require('../../server');
+const server = require('../server');
 
 chai.use(chaiHttp);
 
@@ -12,10 +12,7 @@ describe('/First Test Collection', () => {
         chai.request(server)
         .get('/welcome')
         .end((err, res) => {
-            res.should.have.status(200);
-            res.body.should.be.a('object');    
-            const actualVal = res.body.message;
-            expect(actualVal).to.be.equal("I'm alive...");        
+            expect(res.status).to.be.equal(200);
             done();
         });
     });
