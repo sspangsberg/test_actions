4b5de31c460e15bdf66c19a591c1156c16f4438a
sspangsberg <smsj@easv.dk>
2021-04-02 22:22:29 +0200

    update

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const app = express();
 
 const dotenv = require('dotenv');
@@ -8,19 +7,6 @@ dotenv.config();
 app.get("/welcome", (req,res) => {
     res.status(200).send({message: "I'm alive..."});
   }); 
-
-app.get("/f1", (req,res) => {
-    res.status(200).send({message: "Hey I'm a new feature..."});
-  }); 
-
-//connect to the MongoDB using Mongoose ODM
-mongoose.connect (
-  process.env.DBHOST,  { useUnifiedTopology: true, useNewUrlParser: true }
-).catch(error => console.log("Error connecting to MongoDB: " + error));
-
-mongoose.connection.once('open', () => console.log('Connected succesfully to MongoDB'));
-
-
 
 //Simulate db connection
 console.log("DBHOST:" + process.env.DBHOST);
